ea0a702deee693eee28120b03ae04e88446f5a28
Jadson VSantos <jadsonvenancio1983@gmail.com>
2024-02-12 12:43:10 -0300

    Criei ex001js

---
+++ aula09/ex001.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro e ${vel} Km/h`)
+if (vel > 60) { //condição simples!
+    console.log('Você ultrapassou a velocodade permitida, Multado!')
+}
+console.log('Digija sempre usando cinto de segurança!')
