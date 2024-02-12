84e216be5b18c9fc6204fd333262f799a11625b8
Jadson VSantos <jadsonvenancio1983@gmail.com>
2024-02-12 17:53:14 -0300

    Criei os horários

--- Exercicios/ex012/script.js
+++ Exercicios/ex012/script.js
@@ -1,1 +1,20 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var horas = data.getHours()
+    msg.innerHTML = `Agora são ${horas} horas.`
+    if (horas >= 0 && horas < 12) {
+        //Bom dia!
+        img.src = ('imagemmanha.png')
+        document.body.style.background = '#e2cd9f'
+    }else if(horas >= 12 && horas < 18) {
+        //Boa tarde!
+        img.src = ('imagemtarde.png')
+        document.body.style.background = '#b9836f'
+    }else {
+        //Boa noite!
+        img.src = ('imagemnoite.png')
+        document.body.style.background = '#515154'
+    }
+}
