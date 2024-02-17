9a6c96d941d0a51ecfaea2baf22ad0bd08ea5d95
Jadson VSantos <jadsonvenancio1983@gmail.com>
2024-02-17 10:06:48 -0300

    Criei as funções

---
+++ Exercicios/aula14/funçao01.js
@@ -0,0 +1,11 @@
+function parImpar(n) {
+    if (n%2 == 0) {
+        return 'par!'
+    } else {
+        return 'Impar'
+    }
+}
+/*let res = parImpar(4)
+console.log(res)
+*/
+console.log(parImpar(223))
